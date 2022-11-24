773b9ffe38b42c907735e6460391f0989957131e
Theresa Nguyen <tee.nguyen+github@live.com.au>
2022-11-24 02:37:44 -0800

    update portfolio

---
+++ precache-manifest.67424805a807f6b39ea2203a8e881a57.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b4c7adc48a53e2750d8a7355fbfa65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "714d99b1389ad803395e",
+    "url": "/static/css/main.3a42142b.chunk.css"
+  },
+  {
+    "revision": "0e89935f37826ff06c36",
+    "url": "/static/js/2.01fc7923.chunk.js"
+  },
+  {
+    "revision": "1cdfb040f92bd1a426a6c2510cae2556",
+    "url": "/static/js/2.01fc7923.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "714d99b1389ad803395e",
+    "url": "/static/js/main.e0da8840.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.e0da8840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd5304b19a9f7a1a354b",
+    "url": "/static/js/runtime-main.65c36fb4.js"
+  },
+  {
+    "revision": "f5753d7b393c99da965dc99d6f085f62",
+    "url": "/static/media/bofaml.f5753d7b.jpg"
+  },
+  {
+    "revision": "f32b9925d798a617b4dbb00e00e38048",
+    "url": "/static/media/lr.f32b9925.jpg"
+  },
+  {
+    "revision": "2e1df3ad5fcfcde3e23e6b8d18bfec9e",
+    "url": "/static/media/motif.2e1df3ad.png"
+  },
+  {
+    "revision": "f2b41ec58bfd5475d7410312d27a6f6b",
+    "url": "/static/media/produce48.f2b41ec5.png"
+  },
+  {
+    "revision": "8edcc3375b28c646682dfbe21e55f4fa",
+    "url": "/static/media/rba.8edcc337.jpg"
+  },
+  {
+    "revision": "10ce4d19b6ae4ec040ab0a6af3d02bd7",
+    "url": "/static/media/robot.10ce4d19.svg"
+  },
+  {
+    "revision": "20c2b010c20119f4e7815245573b199e",
+    "url": "/static/media/robot.20c2b010.png"
+  },
+  {
+    "revision": "1b0e957177299ee9eeaca82639ccd41e",
+    "url": "/static/media/spaceship.1b0e9571.svg"
+  },
+  {
+    "revision": "df2454b9cccbcf04a3cbc3a9efdced41",
+    "url": "/static/media/spaceship.df2454b9.png"
+  },
+  {
+    "revision": "5bf0f197be8339c3bfed5a7d0b2a2211",
+    "url": "/static/media/starmap.5bf0f197.png"
+  },
+  {
+    "revision": "430bfe9d876156e0cbf8417a00cf8fc2",
+    "url": "/static/media/welcome.430bfe9d.png"
+  }
+]);
