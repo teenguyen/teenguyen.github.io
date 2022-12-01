457ed9396835cf48e8d30c9b762521dc82f79f4b
Theresa Nguyen <tee.nguyen+github@live.com.au>
2022-12-01 01:46:13 -0800

    update paths

---
+++ projects/produce48-sorter/precache-manifest.bf7f61c62a2c6b6ca8d70091c36a7112.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64b0a09d355844cc67b8de3dc0d11281",
+    "url": "/projects/produce48-sorter/index.html"
+  },
+  {
+    "revision": "8515f55fb7b276ba0406",
+    "url": "/projects/produce48-sorter/static/css/main.3c8d9395.chunk.css"
+  },
+  {
+    "revision": "712ac1059df1244414e0",
+    "url": "/projects/produce48-sorter/static/js/2.9264c4be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/projects/produce48-sorter/static/js/2.9264c4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8515f55fb7b276ba0406",
+    "url": "/projects/produce48-sorter/static/js/main.7d65d50a.chunk.js"
+  },
+  {
+    "revision": "7c4be8006bcf22984023",
+    "url": "/projects/produce48-sorter/static/js/runtime-main.cc1031b6.js"
+  },
+  {
+    "revision": "3a78fc13082cdb03949cdb037b456d02",
+    "url": "/projects/produce48-sorter/static/media/header.3a78fc13.png"
+  },
+  {
+    "revision": "3d9b300fcf30f9978b3d3527f859a996",
+    "url": "/projects/produce48-sorter/static/media/logo.3d9b300f.png"
+  }
+]);
--- projects/produce48-sorter/service-worker.js
+++ projects/produce48-sorter/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/produce48-sorter/precache-manifest.54010b1b5d6ac502fe62a0fb3d5e9aa5.js"
+  "/projects/produce48-sorter/precache-manifest.bf7f61c62a2c6b6ca8d70091c36a7112.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/produce48-sorter/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projects/produce48-sorter/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
