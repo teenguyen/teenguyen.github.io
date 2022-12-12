e662dd2cd71225a52c7cf3b2836c968a24e997a6
Theresa Nguyen <tee.nguyen+github@live.com.au>
2022-12-11 22:17:00 -0800

    add nook-phone wip

---
+++ projects/nook-phone/precache-manifest.0a4c50b0d9e22197b14bc1aa3300b6a5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c559b6b887ab76e929fb2c1205aee357",
+    "url": "/projects/nook-phone/index.html"
+  },
+  {
+    "revision": "840e3f1663d515d024cb",
+    "url": "/projects/nook-phone/static/css/main.aab5d81f.chunk.css"
+  },
+  {
+    "revision": "dfa510102f06f5c079d8",
+    "url": "/projects/nook-phone/static/js/2.c3fc3841.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/projects/nook-phone/static/js/2.c3fc3841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840e3f1663d515d024cb",
+    "url": "/projects/nook-phone/static/js/main.239e400b.chunk.js"
+  },
+  {
+    "revision": "de2b6afb35bb33457174",
+    "url": "/projects/nook-phone/static/js/runtime-main.324585c5.js"
+  },
+  {
+    "revision": "17fb90475ecca4e180a3861c566fdb7a",
+    "url": "/projects/nook-phone/static/media/best-friends-list-app.17fb9047.svg"
+  },
+  {
+    "revision": "d864b1ff04402a4414d237ae9b16e8fe",
+    "url": "/projects/nook-phone/static/media/bg.d864b1ff.jpg"
+  },
+  {
+    "revision": "17db98c6ee83d417ba1e2cbc55e16d83",
+    "url": "/projects/nook-phone/static/media/camera-app.17db98c6.svg"
+  },
+  {
+    "revision": "6bcb7e720a19bc470fa5e857f7cff453",
+    "url": "/projects/nook-phone/static/media/chat-log-app.6bcb7e72.svg"
+  },
+  {
+    "revision": "d484caa7512b560a30db616374b9a25d",
+    "url": "/projects/nook-phone/static/media/critterpedia-app.d484caa7.svg"
+  },
+  {
+    "revision": "c935b6208c1ce9fc302e3c04e1913636",
+    "url": "/projects/nook-phone/static/media/custom-designs-app.c935b620.svg"
+  },
+  {
+    "revision": "9ee1a3789bfb105ee12397e38e5051a9",
+    "url": "/projects/nook-phone/static/media/diy-recipes-app.9ee1a378.svg"
+  },
+  {
+    "revision": "bc3bcf13525a6aa5969b34ae3be3fc7d",
+    "url": "/projects/nook-phone/static/media/location.bc3bcf13.svg"
+  },
+  {
+    "revision": "ad443520222364bf11ce9443c5fdf734",
+    "url": "/projects/nook-phone/static/media/map-app.ad443520.svg"
+  },
+  {
+    "revision": "e3f8e76c58f643c2bddf8b563c06bfce",
+    "url": "/projects/nook-phone/static/media/nook-miles-app.e3f8e76c.svg"
+  },
+  {
+    "revision": "647269f5123cb8145b6606caf83c77dd",
+    "url": "/projects/nook-phone/static/media/nook-phone.647269f5.svg"
+  },
+  {
+    "revision": "1e6f8e1ec0537b1695a4b55a2734bde0",
+    "url": "/projects/nook-phone/static/media/passport-app.1e6f8e1e.svg"
+  },
+  {
+    "revision": "6e4238cfb5d4ff6be49609d4d9a7839e",
+    "url": "/projects/nook-phone/static/media/screenshot.6e4238cf.png"
+  },
+  {
+    "revision": "02b91e4a008ffe3dfcad3a0c0577fea3",
+    "url": "/projects/nook-phone/static/media/wifi.02b91e4a.svg"
+  }
+]);
